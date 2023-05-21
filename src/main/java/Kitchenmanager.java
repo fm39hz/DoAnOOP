bdc286d63443c7e7fd9ad74607e2e4d5ca4cf3c5
fm39hz <hitpoint2k3@gmail.com>
2023-05-21 21:44:54 +0700

    cleanup some junk from dumbshit ide

--- src/main/java/Kitchenmanager.java
+++ src/main/java/Kitchenmanager.java
@@ -4,16 +4,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Kitchenmanager extends JFrame {
-    private JMenuItem ExportCsv;
-    private JMenu File;
-    private JMenu Help;
-    private JMenuItem ImportCsv;
-    private JMenuBar Menu;
-    private JScrollBar ScrollBar;
-    private JScrollPane ViewPanel;
+    private JMenuBar Menu = new JMenuBar();
+    private JMenu File = new JMenu();
+    private JMenu Help = new JMenu();
+    private JMenuItem ExportCsv = new JMenuItem();
+    private JMenuItem ImportCsv = new JMenuItem();
+    private JScrollBar ScrollBar = new JScrollBar();
+    private JScrollPane ViewPanel = new JScrollPane();
 
     public Kitchenmanager() {
-        initComponents();
+        Init();
         }
 
     public static void main(String args[]) {
@@ -29,19 +29,11 @@ public class Kitchenmanager extends JFrame {
             });
         }
 
-    private void initComponents() {
-        ViewPanel = new JScrollPane();
-        ScrollBar = new JScrollBar();
-        Menu = new JMenuBar();
-        File = new JMenu();
-        ExportCsv = new JMenuItem();
-        ImportCsv = new JMenuItem();
-        Help = new JMenu();
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Kitchen Manager");
-        setMinimumSize(new Dimension(1280, 720));
-        setName("MainFrame");
+    private void Init() {
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setTitle("Kitchen Manager");
+        this.setMinimumSize(new Dimension(1280, 720));
+        this.setName("MainFrame");
         
         ViewPanel.addMouseWheelListener(new MouseWheelListener() {
             @Override
@@ -81,29 +73,26 @@ public class Kitchenmanager extends JFrame {
         Help.setText("Help");
         Menu.add(Help);
 
-        setJMenuBar(Menu);
-
+        this.setJMenuBar(Menu);
+        this.SetLayout();
+        this.pack();
+        }
+    private void SetLayout(){
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                    .addContainerGap()
                     .addComponent(ViewPanel, GroupLayout.DEFAULT_SIZE, 1124, Short.MAX_VALUE)
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                     .addComponent(ScrollBar, GroupLayout.PREFERRED_SIZE,
                         GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)));
         layout.setVerticalGroup(
             layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
-                    .addContainerGap()
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addComponent(ViewPanel, GroupLayout.DEFAULT_SIZE, 542,
                             Short.MAX_VALUE)
                         .addComponent(ScrollBar, GroupLayout.DEFAULT_SIZE,
-                            GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addContainerGap()));
-
-        pack();
+                            GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))));
         }
     }
