f883e8f7ce7ee91013a6beea9abf37eb3343a42d
fm39hz <hitpoint2k3@gmail.com>
2023-05-23 19:16:49 +0700

    create database connector

--- src/main/java/team4/KitchenManager/DatabaseConnector.java
+++ src/main/java/team4/KitchenManager/DatabaseConnector.java
@@ -8,21 +8,34 @@ import lombok.Data;
 
 @Data
 public class DatabaseConnector {
-    public class Url{
+    public static class Url{
         public static final String MariaDB = "jdbc:mariadb://localhost:3306/kitchenmanager";
         public static final String MySql = "jdbc:mysql://localhost:3306/kitchenmanager";
         }
-    private Boolean ConnectionState = true;
+    private Boolean ConnectionState;
     private Connection Connector;
 
+    public DatabaseConnector(){
+        this.ConnectionState = false;
+        this.Connect(Url.MariaDB, "root", "");
+        }
     public DatabaseConnector(String url, String userName, String password){
+        this.ConnectionState = false;
+        this.Connect(url, userName, password);
+        }
+        
+    public void Connect(String url, String userName, String password){
         try{
             this.setConnector(DriverManager.getConnection(url, userName, password));
             System.out.println("Connecting to Database through: " + url);
             }
         catch (SQLException CannotGetTable){
             System.out.println("Cannot connect to " + url + " at state " + CannotGetTable.getSQLState());
+            CannotGetTable.printStackTrace();
             this.ConnectionState = false;
+            return;
             }
+        this.ConnectionState = true;
+        System.out.println("Connected to database: " + url);
         }
     }
