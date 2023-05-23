1ef6fede97a0f029fce9fa04fa8cd26523397850
fm39hz <hitpoint2k3@gmail.com>
2023-05-23 19:31:14 +0700

    update property

--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -3,9 +3,10 @@ package team4;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.sql.SQLException;
-import team4.KitchenManager.DatabaseConnector;
-import team4.KitchenManager.DAO.DAO;
-import team4.KitchenManager.DatabaseConnector.Url;
+
+import team4.KitchenManager.DAO.GenericsDAO;
+import team4.KitchenManager.DAO.DatabaseConnector;
+import team4.KitchenManager.DAO.DatabaseConnector.Url;
 import team4.KitchenManager.Model.Customers;
 import team4.KitchenManager.Model.Dishes;
 import team4.KitchenManager.Model.Employees;
@@ -15,12 +16,12 @@ public class AppTest {
     Customers _testcustomer;
     Dishes _testdishes;
     DatabaseConnector _testdb;
-    DAO<Employees> _testdao;
+    GenericsDAO<Employees> _testdao;
     public AppTest(){
         _testEmployees = new Employees(07, "Pham Danh Hien", "0849070703", "Staff", 10000000);
         _testcustomer = new Customers(07, "Pham Danh Hien", "0849070703");
         _testdb = new DatabaseConnector(Url.MariaDB, "root", "");
-        _testdao = new DAO<>();
+        _testdao = new GenericsDAO<>();
         }
     @Test
     public void ConectionTest(){
