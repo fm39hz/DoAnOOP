8926cbc5a741108470c00c2c8eae6caae67f2e6a
fm39hz <hitpoint2k3@gmail.com>
2023-05-23 19:25:48 +0700

    added test for DAO

--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -2,16 +2,37 @@ package team4;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.sql.SQLException;
 import team4.KitchenManager.DatabaseConnector;
+import team4.KitchenManager.DAO.DAO;
 import team4.KitchenManager.DatabaseConnector.Url;
+import team4.KitchenManager.Model.Customers;
+import team4.KitchenManager.Model.Dishes;
+import team4.KitchenManager.Model.Employees;
 
-public class AppTest 
-{
+public class AppTest {
+    Employees _testEmployees;
+    Customers _testcustomer;
+    Dishes _testdishes;
     DatabaseConnector _testdb;
-    @Test
-    public void ConectionTest()
-    {
+    DAO<Employees> _testdao;
+    public AppTest(){
+        _testEmployees = new Employees(07, "Pham Danh Hien", "0849070703", "Staff", 10000000);
+        _testcustomer = new Customers(07, "Pham Danh Hien", "0849070703");
         _testdb = new DatabaseConnector(Url.MariaDB, "root", "");
+        _testdao = new DAO<>();
+        }
+    @Test
+    public void ConectionTest(){
         assertEquals(_testdb.getConnectionState(), true);
+        }
+    @Test
+    public void CustomersTest(){
+        assertEquals(_testcustomer.getID(), 07);
+        }
+    @Test
+    public void DAOTest() throws SQLException{
+        _testdao.Add(_testEmployees);
+        assertEquals(_testdao.getConnector().getConnectionState(), _testdb.getConnectionState());
+        }
     }
-}
