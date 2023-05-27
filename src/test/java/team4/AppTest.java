d9ac8852893647c1db416b2d878c08a5629c25db
fm39hz <hitpoint2k3@gmail.com>
2023-05-27 13:24:01 +0700

    done with DAO example

--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -11,12 +11,14 @@ import team4.KitchenManager.Model.*;
 public class AppTest {
     Employees _testEmployees;
     Customers _testcustomer;
+    Ingredient _testIngredient;
     Dishes _testdishes;
     DatabaseConnector _testdb;
     MainDAO _testdao;
     public AppTest(){
         _testEmployees = new Employees(07, "Pham Danh Hien", "0849070703", "Staff", 10000000);
         _testcustomer = new Customers(07, "Pham Danh Hien", "0849070703");
+        _testIngredient = new Ingredient(7, "Rice", 70, 100);
         _testdb = new DatabaseConnector(Url.MariaDB, "root", "");
         _testdao = new MainDAO();
         }
@@ -30,12 +32,19 @@ public class AppTest {
     //     }
     @Test
     public void DAOTest() throws SQLException{
-        var _temp = _testdao.Get(new Ingredient(), 7);
+        //Kiểm tra trạng thái kết nối tới database
+        assertEquals(_testdao.getConnectionState(), _testdb.getConnectionState());
+        _testdao.Add(_testEmployees);
+        //Lấy ra dữ liệu từ database tương ứng với class được truyền vào
+        var _temp = _testdao.Get(new Employees(), 7);
+            //Khi không tìm thấy đối tượng thỏa mãn sẽ trả về null
             if (_temp == null){
-                _testdao.Add(_temp);
+                //Nếu không tìm thấy đối tượng trong database thì sẽ thêm đối tượng
+                _testdao.Add(_testEmployees);
+                //Lần này khi lấy sẽ không trả về null nữa
+                _temp = _testdao.Get(new Employees(), 7);
                 }
-        assertEquals(_testdao.getConnector().getConnectionState(), _testdb.getConnectionState());
-        _testdao.Remove(_temp);
-        _testdao.Remove(_temp.toString(), 7);
+            //Xóa đối tượng
+            _testdao.Remove(_temp);
         }
     }
