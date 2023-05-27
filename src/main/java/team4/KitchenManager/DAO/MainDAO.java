c1f6e4329b5a9c278bda4fb00faa72ba1014ca16
fm39hz <hitpoint2k3@gmail.com>
2023-05-27 10:29:26 +0700

    finished DAO class

---
+++ src/main/java/team4/KitchenManager/DAO/MainDAO.java
@@ -0,0 +1,135 @@
+package team4.KitchenManager.DAO;
+
+import java.lang.reflect.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import team4.KitchenManager.Model.HasID;
+
+public class MainDAO{
+    private DatabaseConnector Connector;
+    public MainDAO(){
+        this.Connector = new DatabaseConnector();
+        }
+    public MainDAO(String url, String userName, String password){
+        this.Connector = new DatabaseConnector(url, userName, password);
+        }
+    public DatabaseConnector getConnector(){
+        return this.Connector;
+        }
+    public void Add(Object object) throws SQLException {
+        var _class = object.getClass();
+        var _property = _class.getDeclaredFields();
+        var _query  = "INSERT INTO " + object.toString() + " (" + PostStringBuilder(_property);
+        var _statement = Connector.getConnector().prepareStatement(_query);
+            System.out.println(_query);
+            for (int i = 0; i < _property.length; i++) {
+                try {
+                    var _value = _class.getMethod(GetGetterMethod(_property[i].getName())).invoke(object);
+                    _statement = SetValue(_value, _statement, i + 1);
+                    } catch (Exception e) {
+                        System.out.print("Exception occur at " + _property[i].getName());
+                        e.printStackTrace();
+                        }
+                }
+            _statement.executeQuery();
+        }
+    public void Remove(Object object) throws SQLException {
+        try{
+            var _query  = "DELETE FROM " + object.toString() + " WHERE id =?";
+            var _statement = Connector.getConnector().prepareStatement(_query);
+                if (object instanceof HasID){
+                    var _hasIDInstance = (HasID)object;
+                        _statement.setInt(1, _hasIDInstance.GetInstanceID());
+                    }
+                _statement.executeQuery();
+            }
+        catch (Exception e){
+            e.printStackTrace();
+            }
+        }
+    public void Remove(String tableName, int id) throws SQLException{
+        try{
+            var _query = "DELETE FROM " + tableName + "WHERE id = ?";
+            var _statement = Connector.getConnector().prepareStatement(_query);
+                _statement.setInt(1, id);
+                _statement.executeQuery();
+            }
+        catch (Exception exception){
+            exception.printStackTrace();
+            }
+        }
+    public <T> T Get(T tempObject, int id) throws SQLException{
+        var _class = tempObject.getClass();
+        var _property = _class.getDeclaredFields();
+        var _query  = "SELECT * FROM " + tempObject.toString() + " WHERE id = ?";
+        var _statement = Connector.getConnector().prepareStatement(_query);
+            _statement.setInt(1, id);
+        var _resultSet = _statement.executeQuery();
+            System.out.println(_query);
+            while(_resultSet.next()){
+                for (int i = 0; i < _property.length; i++){
+                    try{
+                        var _result = GetValue(tempObject.getClass(), _resultSet, i + 1);
+                        _class.getMethod(GetSetterMethod(_property[i].getName())).invoke(tempObject, _result);
+                        } catch (Exception exception){
+                            exception.printStackTrace();
+                            return null;
+                            }
+                    }
+                }
+        return tempObject;
+        }
+    //Các hàm static để sử dụng nội bộ trong class
+    private PreparedStatement SetValue(Object value, PreparedStatement statement, int index) throws SQLException {
+        var _propertyType = value.getClass();
+            if (_propertyType == String.class){
+                statement.setString(index, (String)value);
+                }
+            else if (_propertyType == Integer.class){
+                statement.setInt(index, (int)value);
+                }
+        return statement;
+        }
+    private static <T> T GetValue(Class<T> _propertyType, ResultSet resultSet, int index) throws SQLException {
+        if (_propertyType == String.class){
+            return (T)resultSet.getString(index);
+            }
+        else if (_propertyType == Integer.class){
+            Integer _value = resultSet.getInt(index);
+            return (T)_value;
+            }
+        return null;
+        }
+    private static String PostStringBuilder(Field[] _property){
+        var _sqlString = "";
+            for (int i = 0 ; i < _property.length; i++){
+                _sqlString += _property[i].getName();
+                if (i < _property.length - 1){
+                    _sqlString += ", ";
+                    }
+                else {
+                    _sqlString += ") VALUES (";
+                    }
+                }
+            for (int i = 0 ; i < _property.length; i++){
+                _sqlString += "?";
+                if (i < _property.length - 1){
+                    _sqlString += ", ";
+                    }
+                else {
+                    _sqlString += ");";
+                    }
+                }
+        return _sqlString;
+        }
+    private static String GetGetterMethod(String fieldName) {
+        return "get" + Upcase(fieldName);
+        }
+    private static String GetSetterMethod(String fieldName) {
+        return "set" + Upcase(fieldName);
+        }
+    private static String Upcase(String fieldName){
+        return fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+        }
+    }
--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -3,37 +3,43 @@ package team4;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.sql.SQLException;
-
-import team4.KitchenManager.DAO.GenericsDAO;
+import team4.KitchenManager.DAO.MainDAO;
 import team4.KitchenManager.DAO.DatabaseConnector;
 import team4.KitchenManager.DAO.DatabaseConnector.Url;
+import team4.KitchenManager.Model.Attendance;
 import team4.KitchenManager.Model.Customers;
 import team4.KitchenManager.Model.Dishes;
 import team4.KitchenManager.Model.Employees;
+import team4.KitchenManager.Model.Ingredient;
 
 public class AppTest {
     Employees _testEmployees;
     Customers _testcustomer;
     Dishes _testdishes;
     DatabaseConnector _testdb;
-    GenericsDAO<Employees> _testdao;
+    MainDAO _testdao;
     public AppTest(){
         _testEmployees = new Employees(07, "Pham Danh Hien", "0849070703", "Staff", 10000000);
         _testcustomer = new Customers(07, "Pham Danh Hien", "0849070703");
         _testdb = new DatabaseConnector(Url.MariaDB, "root", "");
-        _testdao = new GenericsDAO<>();
+        _testdao = new MainDAO();
         }
-    @Test
-    public void ConectionTest(){
-        assertEquals(_testdb.getConnectionState(), true);
-        }
-    @Test
-    public void CustomersTest(){
-        assertEquals(_testcustomer.getID(), 07);
-        }
+    // @Test
+    // public void ConectionTest(){
+    //     assertEquals(_testdb.getConnectionState(), true);
+    //     }
+    // @Test
+    // public void CustomersTest(){
+    //     assertEquals(_testcustomer.getID(), 07);
+    //     }
     @Test
     public void DAOTest() throws SQLException{
-        _testdao.Add(_testEmployees);
+        var _temp = _testdao.Get(new Ingredient(), 7);
+            if (_temp == null){
+                _testdao.Add(_temp);
+                }
         assertEquals(_testdao.getConnector().getConnectionState(), _testdb.getConnectionState());
+        _testdao.Remove(_temp);
+        _testdao.Remove(_temp.toString(), 7);
         }
     }
