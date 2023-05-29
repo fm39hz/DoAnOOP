4b9b2fa29b07004945132f79fa630824e3e1a746
fm39hz <hitpoint2k3@gmail.com>
2023-05-29 22:52:10 +0700

    added InDate to store the Date Import

--- src/main/java/team4/KitchenManager/Model/Ingredient.java
+++ src/main/java/team4/KitchenManager/Model/Ingredient.java
@@ -1,5 +1,7 @@
 package team4.KitchenManager.Model;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class Ingredient implements HasID{
     private int ID;
     private String Name;
+    private Date InDate;
     private int InStock;
     private int Cost;
     @Override
