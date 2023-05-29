b4f0125fed35a0cad94d43ee3dbcc357a608e269
fm39hz <hitpoint2k3@gmail.com>
2023-05-29 22:52:44 +0700

    test newly added function today

--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.sql.Date;
+// import java.sql.Date;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import team4.KitchenManager.DAO.MainDAO;
 import team4.KitchenManager.DAO.DatabaseConnector;
 import team4.KitchenManager.DAO.DatabaseConnector.Url;
 import team4.KitchenManager.Model.*;
+import team4.KitchenManager.Service.ComponentTable;
+import team4.KitchenManager.Service.PropertyInfoExtractor;
 
 public class AppTest {
     Employees _testEmployees;
@@ -20,7 +25,7 @@ public class AppTest {
     public AppTest(){
         _testEmployees = new Employees(07, "Pham Danh Hien", "0849070703", "Staff", 10000000);
         _testcustomer = new Customers(07, "Pham Danh Hien", "0849070703");
-        _testIngredient = new Ingredient(7, "Rice", Date.valueOf("07-07-2022"), 70, 100);
+        _testIngredient = new Ingredient(7, "Rice", Date.valueOf("2022-07-07"), 70, 100);
         _testdb = new DatabaseConnector(Url.MariaDB, "root", "");
         _testdao = new MainDAO(_testdb);
         }
@@ -48,5 +53,26 @@ public class AppTest {
                 }
             //Xóa đối tượng
             _testdao.Remove(_temp);
+        }
+    @Test
+    public void PIETest(){
+        var _tempStrings = PropertyInfoExtractor.GetObjectPropertyNames(Employees.class);
+        var _tempTypes = PropertyInfoExtractor.GetObjectPropertyTypes(Employees.class);
+        var _tempValue = PropertyInfoExtractor.InvokeGetter(Employees.class, _tempStrings[3], _testEmployees);
+        var i = 0;
+            for (String _name : _tempStrings){
+                System.out.println(_name + " with class " + _tempTypes[i++]);
+                }
+            System.out.println(_tempValue);
+        assertEquals(_tempValue != null, true);
+        }
+    @Test
+    public void ComponentTableTest(){
+        var _long = new Employees(01, "Ngo Tien Long", "0912222003", "Staff", 12000000);
+        var _empList = new ArrayList<>();
+            _empList.add(_testEmployees);
+            _empList.add(_long);
+        var _tempTable = new ComponentTable<>(Employees.class, _empList);
+            System.out.println(_tempTable.getValueAt(0, 4));
         }
     }
