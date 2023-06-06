02c5655ce7653a9fcc9f072b398e5f8042892b60
fm39hz <hitpoint2k3@gmail.com>
2023-06-07 00:04:19 +0700

    Create Class to Contain Id and Name data

---
+++ src/main/java/team4/KitchenManager/Model/InformationSchema.java
@@ -0,0 +1,11 @@
+package team4.KitchenManager.Model;
+
+import lombok.Data;
+
+@Data
+public class InformationSchema {
+    private String Id;
+    private String FirstName;
+    private String LastName;
+    private String PhoneNumber;
+    }
