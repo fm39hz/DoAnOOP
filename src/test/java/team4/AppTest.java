35fce010946f5bc0a0eeed5fde2deb14b860c129
fm39hz <hitpoint2k3@gmail.com>
2023-06-08 22:19:38 +0700

    create a test for dishes

--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -2,6 +2,8 @@ package team4;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
+import team4.KitchenManager.Controller.DishesController;
 import team4.KitchenManager.DAO.DatabaseConnector;
 import team4.KitchenManager.DAO.DatabaseConnector.Url;
 import team4.KitchenManager.Model.Dish;
@@ -17,16 +19,15 @@ public class AppTest {
     public void ConectionTest(){
         assertEquals(_testdb.getConnectionState(), true);
         }
-    }
     @Test
-    public void Dishes() throws Exception {
+    public void DishesTest() throws Exception {
         var _dishes = new DishesController();
-//        var _getAll = _dishes.sortBy("nhame",false);
-//
-//        for (Dish _dish: _getAll) {
-//            System.out.println(_dish.getID()+"-"+_dish.getName()+"-"+_dish.getPrice());
-//        }
+        var _getAll = _dishes.sortBy("nhame",false);
+            for (Dish _dish: _getAll) {
+                System.out.println(_dish.getID()+"-"+_dish.getName()+"-"+_dish.getPrice());
+                }
         var _remain = _dishes.calculateRemaining("200");
-        System.out.println(_remain);
+            assertEquals(_remain, 30000);
+            System.out.println(_remain);
     }
 }
