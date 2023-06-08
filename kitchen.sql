764534c20525ea902022bb5bc2aaefac8e517d8e
fm39hz <hitpoint2k3@gmail.com>
2023-06-08 22:38:36 +0700

    fixed db error

--- kitchen.sql
+++ kitchen.sql
@@ -14,6 +14,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for kitchen
+DROP DATABASE IF EXISTS `kitchen`;
+CREATE DATABASE IF NOT EXISTS `kitchen` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `kitchen`;
+
+-- Dumping structure for table kitchen.attendances
+DROP TABLE IF EXISTS `attendances`;
+CREATE TABLE IF NOT EXISTS `attendances` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `emp_id` int(11) NOT NULL,
+  `day` date NOT NULL,
+  `checkin_at` datetime NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_emp_Id` (`emp_id`) USING BTREE,
+  CONSTRAINT `FK_emp_Id` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
 -- Dumping data for table kitchen.attendances: ~13 rows (approximately)
 INSERT INTO `attendances` (`Id`, `emp_id`, `day`, `checkin_at`) VALUES
 	(100, 401, '2023-06-15', '2023-06-01 15:07:05'),
@@ -29,6 +47,15 @@ INSERT INTO `attendances` (`Id`, `emp_id`, `day`, `checkin_at`) VALUES
 	(112, 411, '2023-06-08', '2023-06-08 20:13:41'),
 	(113, 410, '2023-06-02', '2023-06-02 20:13:41'),
 	(114, 409, '2023-06-01', '2023-06-01 15:13:30');
+
+-- Dumping structure for table kitchen.customers
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `phone` text DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=317 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 -- Dumping data for table kitchen.customers: ~14 rows (approximately)
 INSERT INTO `customers` (`Id`, `name`, `phone`) VALUES
@@ -47,6 +74,16 @@ INSERT INTO `customers` (`Id`, `name`, `phone`) VALUES
 	(312, 'Kim', '1234512345'),
 	(313, 'Huy', '12341234');
 
+-- Dumping structure for table kitchen.dishes
+DROP TABLE IF EXISTS `dishes`;
+CREATE TABLE IF NOT EXISTS `dishes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `cost` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=212 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
 -- Dumping data for table kitchen.dishes: ~11 rows (approximately)
 INSERT INTO `dishes` (`id`, `name`, `cost`, `price`) VALUES
 	(200, 'Cơm rang dưa bò', 20000, 45000),
@@ -60,6 +97,17 @@ INSERT INTO `dishes` (`id`, `name`, `cost`, `price`) VALUES
 	(208, 'Cơm sườn sốt', 20000, 45000),
 	(209, 'Cơm rang gà chiên', 20000, 45000),
 	(210, 'Cơm rang trứng', 10000, 25000);
+
+-- Dumping structure for table kitchen.employees
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE IF NOT EXISTS `employees` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `phone` text NOT NULL,
+  `position` text NOT NULL,
+  `salary` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=413 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 -- Dumping data for table kitchen.employees: ~12 rows (approximately)
 INSERT INTO `employees` (`Id`, `name`, `phone`, `position`, `salary`) VALUES
@@ -76,6 +124,17 @@ INSERT INTO `employees` (`Id`, `name`, `phone`, `position`, `salary`) VALUES
 	(410, 'Cam', '151243124', 'NV', 4000),
 	(411, 'Kinh', '12352345', 'PB', 5000);
 
+-- Dumping structure for table kitchen.ingredients
+DROP TABLE IF EXISTS `ingredients`;
+CREATE TABLE IF NOT EXISTS `ingredients` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `date_in` date NOT NULL,
+  `in_stock` int(11) NOT NULL,
+  `cost` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=612 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
 -- Dumping data for table kitchen.ingredients: ~11 rows (approximately)
 INSERT INTO `ingredients` (`Id`, `name`, `date_in`, `in_stock`, `cost`) VALUES
 	(600, 'thịt bò', '2023-06-14', 30000, 150000),
@@ -90,15 +149,53 @@ INSERT INTO `ingredients` (`Id`, `name`, `date_in`, `in_stock`, `cost`) VALUES
 	(609, 'thịt gà', '2023-06-22', 60000, 120000),
 	(610, 'sữa', '2023-06-23', 70000, 20000);
 
+-- Dumping structure for table kitchen.orders
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `createdDay` date NOT NULL,
+  `createdTime` time NOT NULL,
+  `cus_Id` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `cus_Id` (`cus_Id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`cus_Id`) REFERENCES `customers` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=713 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
 -- Dumping data for table kitchen.orders: ~1 rows (approximately)
 INSERT INTO `orders` (`Id`, `createdDay`, `createdTime`, `cus_Id`) VALUES
 	(701, '2023-06-08', '11:49:06', 301);
+
+-- Dumping structure for table kitchen.order_detail
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE IF NOT EXISTS `order_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `dishes_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__orders` (`order_id`),
+  KEY `FK__dishes` (`dishes_id`),
+  CONSTRAINT `FK__dishes` FOREIGN KEY (`dishes_id`) REFERENCES `dishes` (`id`),
+  CONSTRAINT `FK__orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table kitchen.order_detail: ~3 rows (approximately)
 INSERT INTO `order_detail` (`id`, `order_id`, `dishes_id`, `quantity`) VALUES
 	(1, 701, 201, 1),
 	(2, 701, 203, 2),
 	(3, 701, 206, 2);
+
+-- Dumping structure for table kitchen.quantities
+DROP TABLE IF EXISTS `quantities`;
+CREATE TABLE IF NOT EXISTS `quantities` (
+  `dish_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`dish_id`,`ingredient_id`) USING BTREE,
+  KEY `FK_quantities_ingredients` (`ingredient_id`),
+  CONSTRAINT `FK_quantities_dishes` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`),
+  CONSTRAINT `FK_quantities_ingredients` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 -- Dumping data for table kitchen.quantities: ~2 rows (approximately)
 INSERT INTO `quantities` (`dish_id`, `ingredient_id`, `quantity`) VALUES
