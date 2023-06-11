89032103ca52f02b854268e90b61f5bc0abac7d5
fm39hz <hitpoint2k3@gmail.com>
2023-06-11 12:05:56 +0700

    remove unecessary using

--- src/test/java/team4/AppTest.java
+++ src/test/java/team4/AppTest.java
@@ -3,12 +3,9 @@ package team4;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-import team4.KitchenManager.Controller.DishesController;
-import team4.KitchenManager.Controller.IngredientController;
+import team4.KitchenManager.Controller.*;
 import team4.KitchenManager.DAO.DatabaseConnector;
 import team4.KitchenManager.DAO.DatabaseConnector.Url;
-import team4.KitchenManager.Model.Dish;
-// import team4.KitchenManager.Model.*;
 
 public class AppTest {
     // Ingredient _testIngredient;
@@ -24,7 +21,7 @@ public class AppTest {
     public void DishesTest() throws Exception {
         var _dishes = new DishesController(_testdb);
         var _getAll = _dishes.sortBy("name",false);
-            for (Dish _dish: _getAll) {
+            for (var _dish: _getAll) {
                 System.out.println(_dish.getID()+"|"+_dish.getName()+"|"+_dish.getPrice());
                 }
         var _remain = _dishes.calculateRemaining("200");
